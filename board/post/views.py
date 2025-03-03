271650a31f547e268cd5d3fb4f496c973ea15101
mokkachina <maxiklim80@gmail.com>
2025-03-03 15:21:58 +0600

    add venv to gitignore

--- board/post/views.py
+++ board/post/views.py
@@ -4,13 +4,20 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
+menu = [
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'add_page'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'},
+]
 data_db = [
     {'id': 1, 'title': "Автомобили", 'content': "ВАЗ", 'is_published': True},
     {'id': 2, 'title': "Бытовые приборы", 'content': "Миксер", 'is_published': True},
     {'id': 3, 'title': "Дача", 'content': "Рассада", 'is_published': True}
 ]
+
+
 
 def index(reqest):
     # t = render_to_string('board/index.html')
@@ -23,19 +30,18 @@ def index(reqest):
     return render(reqest, 'board/index.html', context=data)
 
 def about(reqest):
-    return render(reqest, 'board/about.html', {"title":"О сайте"})
+    return render(reqest, 'board/about.html', {"title":"О сайте", 'menu': menu})
 
-def categories(reqest, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def show_post(reqest, post_id):
+    return HttpResponse(f'Отображение статьи с id ={post_id}')
 
-def categories_by_slug(reqest, cat_slug):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def addpage(request):
+    return HttpResponse('Добавление статьи')
+def contact(request):
+    return HttpResponse('Обратная связь')
+def login(request):
+    return HttpResponse('Авторизация')
 
-def archive(reqest, year):
-    if year > 2025:
-        uri = reverse('cats', args=('home_device',))
-        return redirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def page_not_found(reqest, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
